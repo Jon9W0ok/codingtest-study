df6135a17e665e17e01044845e26b33d37328a97
yesosj <89458649+yesosj@users.noreply.github.com>
2025-03-21 21:21:03 +0900

    Update 피로도.cpp

--- Oh/1주차/피로도.cpp
+++ Oh/1주차/피로도.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define MAX 8
-
 int answer = 0;
-bool visited[MAX] = {0};
+bool visited[8] = {0};
 
-void dfs(int cnt, int k, vector<vector<int>> &dungeons) {
-    if (cnt > answer) answer = cnt;
+void dfs(int cnt, int k, vector<vector<int>> &dungeons){
+    if(cnt > answer) answer = cnt;
     
-    for (int i=0; i<dungeons.size(); i++) {
-        if (!visited[i] && dungeons[i][0] <= k) {
-            visited[i] = true;
-            dfs(cnt + 1, k - dungeons[i][1], dungeons);
-            visited[i] = false;
+    for(int i=0; i<dungeons.size(); i++){
+        if(!visited[i] && k - dungeons[i][0] >=0){
+            visited[i] = 1;
+          
+            dfs(cnt+1, k - dungeons[i][1], dungeons);
+            visited[i] = 0;
         }
     }
 }
 
-int solution(int k, vector<vector<int>> dungeons) {
+int solution(int k, vector<vector<int>> dungeons){
     dfs(0, k, dungeons);
-    
     return answer;
 }
 
